f28ba6347966a4371a8524462a46e572465a983b
Liam Galvin <liam.galvin@aquasec.com>
2022-01-21 11:04:03 +0000

    fix google firewall

--- rules/google/compute/no_public_egress.tf.go
+++ rules/google/compute/no_public_egress.tf.go
@@ -1,21 +1,27 @@
 package compute
 
 var terraformNoPublicEgressGoodExamples = []string{
-        `
+	`
  resource "google_compute_firewall" "good_example" {
- 	destination_ranges = ["1.2.3.4/32"]
- }`,
+  allow {
+    protocol = "icmp"
+  }
+  destination_ranges = ["1.2.3.4/32"]
+}`,
 }
 
 var terraformNoPublicEgressBadExamples = []string{
-        `
- resource "google_compute_firewall" "bad_example" {
- 	destination_ranges = ["0.0.0.0/0"]
- }`,
+	`
+resource "google_compute_firewall" "bad_example" {
+  allow {
+    protocol = "icmp"
+  }
+  destination_ranges = ["0.0.0.0/0"]
+}`,
 }
 
 var terraformNoPublicEgressLinks = []string{
-        `https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_firewall`,
+	`https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_firewall`,
 }
 
 var terraformNoPublicEgressRemediationMarkdown = ``
