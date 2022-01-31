5b50e33067cdcbdebb84ba13b0cc37af65aa6c8a
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-01-31 12:23:43 +0100

    feat: add unit tests for AWS Athena rules (#143)

--- rules/aws/athena/no_encryption_override_test.go
+++ rules/aws/athena/no_encryption_override_test.go
@@ -6,24 +6,40 @@ import (
 	"github.com/aquasecurity/defsec/provider/aws/athena"
 	"github.com/aquasecurity/defsec/rules"
 	"github.com/aquasecurity/defsec/state"
+	"github.com/aquasecurity/defsec/types"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCheckNoEncryptionOverride(t *testing.T) {
-	t.SkipNow()
 	tests := []struct {
 		name     string
 		input    athena.Athena
 		expected bool
 	}{
 		{
-			name:     "positive result",
-			input:    athena.Athena{},
+			name: "AWS Athena workgroup doesn't enforce configuration",
+			input: athena.Athena{
+				Metadata: types.NewTestMetadata(),
+				Workgroups: []athena.Workgroup{
+					{
+						Metadata:             types.NewTestMetadata(),
+						EnforceConfiguration: types.Bool(false, types.NewTestMetadata()),
+					},
+				},
+			},
 			expected: true,
 		},
 		{
-			name:     "negative result",
-			input:    athena.Athena{},
+			name: "AWS Athena workgroup enforces configuration",
+			input: athena.Athena{
+				Metadata: types.NewTestMetadata(),
+				Workgroups: []athena.Workgroup{
+					{
+						Metadata:             types.NewTestMetadata(),
+						EnforceConfiguration: types.Bool(true, types.NewTestMetadata()),
+					},
+				},
+			},
 			expected: false,
 		},
 	}
