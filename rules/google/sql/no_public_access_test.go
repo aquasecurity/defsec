563574d5eb955b67fd5eb1280ca10f9671d36971
Liam Galvin <liam.galvin@aquasec.com>
2022-02-15 17:01:50 +0000

    chore: move imports

--- rules/google/sql/no_public_access_test.go
+++ rules/google/sql/no_public_access_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/aquasecurity/defsec/provider/google/sql"
 	"github.com/aquasecurity/defsec/rules"
 	"github.com/aquasecurity/defsec/state"
-	"github.com/aquasecurity/defsec/types"
+	"github.com/aquasecurity/trivy-config-parsers/types"
 	"github.com/stretchr/testify/assert"
 )
 
