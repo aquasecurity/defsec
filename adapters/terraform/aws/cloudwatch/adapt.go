970702155eaa879a630334e68a71aed7aa16a968
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-03-04 10:10:38 +0100

    test: aws cloudwatch tf adapter (#297)

--- adapters/terraform/aws/cloudwatch/adapt.go
+++ adapters/terraform/aws/cloudwatch/adapt.go
@@ -2,6 +2,7 @@ package cloudwatch
 
 import (
 	"github.com/aquasecurity/defsec/parsers/terraform"
+	"github.com/aquasecurity/defsec/parsers/types"
 	"github.com/aquasecurity/defsec/providers/aws/cloudwatch"
 )
 
@@ -15,18 +16,22 @@ func adaptLogGroups(modules terraform.Modules) []cloudwatch.LogGroup {
 	var logGroups []cloudwatch.LogGroup
 	for _, module := range modules {
 		for _, resource := range module.GetResourcesByType("aws_cloudwatch_log_group") {
-			logGroups = append(logGroups, adaptLogGroup(resource))
+			logGroups = append(logGroups, adaptLogGroup(resource, module))
 		}
 	}
 	return logGroups
 }
 
-func adaptLogGroup(resource *terraform.Block) cloudwatch.LogGroup {
+func adaptLogGroup(resource *terraform.Block, module *terraform.Module) cloudwatch.LogGroup {
 	nameAttr := resource.GetAttribute("name")
 	nameVal := nameAttr.AsStringValueOrDefault("", resource)
 
 	KMSKeyIDAttr := resource.GetAttribute("kms_key_id")
 	KMSKeyIDVal := KMSKeyIDAttr.AsStringValueOrDefault("", resource)
+
+	if keyBlock, err := module.GetReferencedBlock(KMSKeyIDAttr, resource); err == nil {
+		KMSKeyIDVal = types.String(keyBlock.FullName(), keyBlock.GetMetadata())
+	}
 
 	retentionInDaysAttr := resource.GetAttribute("retention_in_days")
 	retentionInDaysVal := retentionInDaysAttr.AsIntValueOrDefault(0, resource)
