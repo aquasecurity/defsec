e2a8713b7d8039bdd36efb62de8100aa73b81cc6
Owen Rumney <owen.rumney@aquasec.com>
2022-03-04 16:32:26 +0000

    fix: handle archived repositories for vuln check

--- providers/github/repositories.go
+++ providers/github/repositories.go
@@ -6,4 +6,9 @@ type Repository struct {
 	types.Metadata
 	Public              types.BoolValue
 	VulnerabilityAlerts types.BoolValue
+	Archived            types.BoolValue
+}
+
+func (r Repository) IsArchived() bool {
+	return r.Archived.IsTrue()
 }
