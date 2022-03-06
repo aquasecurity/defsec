4c124fa67b9a184f68a1464809a4bb443c2239ff
Vanesa Sejdiu <vsejdiu4@gmail.com>
2022-03-06 22:14:13 +0100

    test: add tests for azure synapse tf adapters

--- adapters/terraform/azure/synapse/adapt_test.go
+++ adapters/terraform/azure/synapse/adapt_test.go
@@ -4,79 +4,53 @@ import (
 	"testing"
 
 	"github.com/aquasecurity/defsec/adapters/terraform/testutil"
+	"github.com/aquasecurity/defsec/parsers/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/aquasecurity/defsec/providers/azure/synapse"
 )
 
-func Test_Adapt(t *testing.T) {
-	t.SkipNow()
-	tests := []struct {
-		name      string
-		terraform string
-		expected  synapse.Synapse
-	}{
-		{
-			name: "basic",
-			terraform: `
-resource "" "example" {
-    
-}
-`,
-			expected: synapse.Synapse{},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			modules := testutil.CreateModulesFromSource(test.terraform, ".tf", t)
-			adapted := Adapt(modules)
-			testutil.AssertDefsecEqual(t, test.expected, adapted)
-		})
-	}
-}
-
-func Test_adaptWorkspaces(t *testing.T) {
-	t.SkipNow()
-	tests := []struct {
-		name      string
-		terraform string
-		expected  []synapse.Workspace
-	}{
-		{
-			name: "basic",
-			terraform: `
-resource "" "example" {
-    
-}
-`,
-			expected: []synapse.Workspace{},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			modules := testutil.CreateModulesFromSource(test.terraform, ".tf", t)
-			adapted := adaptWorkspaces(modules)
-			testutil.AssertDefsecEqual(t, test.expected, adapted)
-		})
-	}
-}
-
 func Test_adaptWorkspace(t *testing.T) {
-	t.SkipNow()
 	tests := []struct {
 		name      string
 		terraform string
 		expected  synapse.Workspace
 	}{
 		{
-			name: "basic",
+			name: "enabled",
 			terraform: `
-resource "" "example" {
-    
-}
+			resource "azurerm_synapse_workspace" "example" {
+				managed_virtual_network_enabled	   = true
+			}
 `,
-			expected: synapse.Workspace{},
+			expected: synapse.Workspace{
+				Metadata:                    types.NewTestMetadata(),
+				EnableManagedVirtualNetwork: types.Bool(true, types.NewTestMetadata()),
+			},
+		},
+		{
+			name: "disabled",
+			terraform: `
+			resource "azurerm_synapse_workspace" "example" {
+				managed_virtual_network_enabled	   = false
+			}
+`,
+			expected: synapse.Workspace{
+				Metadata:                    types.NewTestMetadata(),
+				EnableManagedVirtualNetwork: types.Bool(false, types.NewTestMetadata()),
+			},
+		},
+		{
+			name: "default",
+			terraform: `
+			resource "azurerm_synapse_workspace" "example" {
+			}
+`,
+			expected: synapse.Workspace{
+				Metadata:                    types.NewTestMetadata(),
+				EnableManagedVirtualNetwork: types.Bool(false, types.NewTestMetadata()),
+			},
 		},
 	}
 
@@ -87,4 +61,20 @@ resource "" "example" {
 			testutil.AssertDefsecEqual(t, test.expected, adapted)
 		})
 	}
+}
+
+func TestLines(t *testing.T) {
+	src := `
+	resource "azurerm_synapse_workspace" "example" {
+		managed_virtual_network_enabled	   = true
+	  }`
+
+	modules := testutil.CreateModulesFromSource(src, ".tf", t)
+	adapted := Adapt(modules)
+
+	require.Len(t, adapted.Workspaces, 1)
+	workspace := adapted.Workspaces[0]
+
+	assert.Equal(t, 3, workspace.EnableManagedVirtualNetwork.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 3, workspace.EnableManagedVirtualNetwork.GetMetadata().Range().GetEndLine())
 }
