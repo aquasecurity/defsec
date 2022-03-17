5d01495d53b8dc246d1a5f90caad2257f32eb205
Liam Galvin <liam.galvin@aquasec.com>
2022-03-17 13:42:43 +0000

    feat: Add more flexible return possibilities for rego rules

--- rego/result.go
+++ rego/result.go
@@ -31,25 +31,42 @@ func (r regoResult) GetRawValue() interface{} {
 func parseResult(raw interface{}) *regoResult {
 	var result regoResult
 	switch val := raw.(type) {
+	case []interface{}:
+		var msg string
+		for _, item := range val {
+			switch raw := item.(type) {
+			case map[string]interface{}:
+				result = parseCause(raw)
+			case string:
+				msg = raw
+			}
+		}
+		result.Message = msg
 	case string:
 		result.Message = val
 	case map[string]interface{}:
-		if msg, ok := val["msg"]; ok {
-			result.Message = fmt.Sprintf("%s", msg)
-		}
-		if filepath, ok := val["filepath"]; ok {
-			result.Filepath = fmt.Sprintf("%s", filepath)
-		}
-		if start, ok := val["startline"]; ok {
-			result.StartLine = parseLineNumber(start)
-		}
-		if end, ok := val["endline"]; ok {
-			result.EndLine = parseLineNumber(end)
-		}
+		result = parseCause(val)
 	default:
 		result.Message = "Rego policy resulted in DENY"
 	}
 	return &result
+}
+
+func parseCause(cause map[string]interface{}) regoResult {
+	var result regoResult
+	if msg, ok := cause["msg"]; ok {
+		result.Message = fmt.Sprintf("%s", msg)
+	}
+	if filepath, ok := cause["filepath"]; ok {
+		result.Filepath = fmt.Sprintf("%s", filepath)
+	}
+	if start, ok := cause["startline"]; ok {
+		result.StartLine = parseLineNumber(start)
+	}
+	if end, ok := cause["endline"]; ok {
+		result.EndLine = parseLineNumber(end)
+	}
+	return result
 }
 
 func parseLineNumber(raw interface{}) int {
