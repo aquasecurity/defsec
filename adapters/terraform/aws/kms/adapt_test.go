d7367b18a05b0af51d3ce59f16de5545b284ff78
Vanesa Sejdiu <vsejdiu4@gmail.com>
2022-03-28 00:25:40 +0200

    test: add tests for aws kms tf adapters

--- adapters/terraform/aws/kms/adapt_test.go
+++ adapters/terraform/aws/kms/adapt_test.go
@@ -4,79 +4,41 @@ import (
 	"testing"
 
 	"github.com/aquasecurity/defsec/adapters/terraform/testutil"
-
+	"github.com/aquasecurity/defsec/parsers/types"
 	"github.com/aquasecurity/defsec/providers/aws/kms"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func Test_Adapt(t *testing.T) {
-	t.SkipNow()
-	tests := []struct {
-		name      string
-		terraform string
-		expected  kms.KMS
-	}{
-		{
-			name: "basic",
-			terraform: `
-resource "" "example" {
-    
-}
-`,
-			expected: kms.KMS{},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			modules := testutil.CreateModulesFromSource(test.terraform, ".tf", t)
-			adapted := Adapt(modules)
-			testutil.AssertDefsecEqual(t, test.expected, adapted)
-		})
-	}
-}
-
-func Test_adaptKeys(t *testing.T) {
-	t.SkipNow()
-	tests := []struct {
-		name      string
-		terraform string
-		expected  []kms.Key
-	}{
-		{
-			name: "basic",
-			terraform: `
-resource "" "example" {
-    
-}
-`,
-			expected: []kms.Key{},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			modules := testutil.CreateModulesFromSource(test.terraform, ".tf", t)
-			adapted := adaptKeys(modules)
-			testutil.AssertDefsecEqual(t, test.expected, adapted)
-		})
-	}
-}
-
 func Test_adaptKey(t *testing.T) {
-	t.SkipNow()
 	tests := []struct {
 		name      string
 		terraform string
 		expected  kms.Key
 	}{
 		{
-			name: "basic",
+			name: "configured",
 			terraform: `
-resource "" "example" {
-    
-}
+			resource "aws_kms_key" "example" {
+				enable_key_rotation = true
+				key_usage = SIGN_VERIFY
+			}
 `,
-			expected: kms.Key{},
+			expected: kms.Key{
+				Usage:           types.String(kms.KeyUsageSignAndVerify, types.NewTestMetadata()),
+				RotationEnabled: types.Bool(true, types.NewTestMetadata()),
+			},
+		},
+		{
+			name: "defaults",
+			terraform: `
+			resource "aws_kms_key" "example" {
+			}
+`,
+			expected: kms.Key{
+				Usage:           types.String("ENCRYPT_DECRYPT", types.NewTestMetadata()),
+				RotationEnabled: types.Bool(false, types.NewTestMetadata()),
+			},
 		},
 	}
 
@@ -87,4 +49,28 @@ resource "" "example" {
 			testutil.AssertDefsecEqual(t, test.expected, adapted)
 		})
 	}
+}
+
+func TestLines(t *testing.T) {
+	src := `
+	resource "aws_kms_key" "example" {
+		enable_key_rotation = true
+		key_usage = SIGN_VERIFY
+	}`
+
+	modules := testutil.CreateModulesFromSource(src, ".tf", t)
+	adapted := Adapt(modules)
+
+	require.Len(t, adapted.Keys, 1)
+	key := adapted.Keys[0]
+
+	assert.Equal(t, 2, key.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 5, key.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 3, key.RotationEnabled.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 3, key.RotationEnabled.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 4, key.Usage.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 4, key.Usage.GetMetadata().Range().GetEndLine())
+
 }
