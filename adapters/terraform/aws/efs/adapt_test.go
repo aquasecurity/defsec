9c54c6e8934be35e3c54855b0d0b9e6e6b1edd06
Vanesa Sejdiu <vsejdiu4@gmail.com>
2022-03-27 19:47:45 +0200

    test: add tests for aws efs tf adapters

--- adapters/terraform/aws/efs/adapt_test.go
+++ adapters/terraform/aws/efs/adapt_test.go
@@ -4,79 +4,43 @@ import (
 	"testing"
 
 	"github.com/aquasecurity/defsec/adapters/terraform/testutil"
+	"github.com/aquasecurity/defsec/parsers/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/aquasecurity/defsec/providers/aws/efs"
 )
 
-func Test_Adapt(t *testing.T) {
-	t.SkipNow()
-	tests := []struct {
-		name      string
-		terraform string
-		expected  efs.EFS
-	}{
-		{
-			name: "basic",
-			terraform: `
-resource "" "example" {
-    
-}
-`,
-			expected: efs.EFS{},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			modules := testutil.CreateModulesFromSource(test.terraform, ".tf", t)
-			adapted := Adapt(modules)
-			testutil.AssertDefsecEqual(t, test.expected, adapted)
-		})
-	}
-}
-
-func Test_adaptFileSystems(t *testing.T) {
-	t.SkipNow()
-	tests := []struct {
-		name      string
-		terraform string
-		expected  []efs.FileSystem
-	}{
-		{
-			name: "basic",
-			terraform: `
-resource "" "example" {
-    
-}
-`,
-			expected: []efs.FileSystem{},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			modules := testutil.CreateModulesFromSource(test.terraform, ".tf", t)
-			adapted := adaptFileSystems(modules)
-			testutil.AssertDefsecEqual(t, test.expected, adapted)
-		})
-	}
-}
-
 func Test_adaptFileSystem(t *testing.T) {
-	t.SkipNow()
 	tests := []struct {
 		name      string
 		terraform string
 		expected  efs.FileSystem
 	}{
 		{
-			name: "basic",
+			name: "configured",
 			terraform: `
-resource "" "example" {
-    
-}
+			resource "aws_efs_file_system" "example" {
+				name       = "bar"
+				encrypted  = true
+				kms_key_id = "my_kms_key"
+			  }
 `,
-			expected: efs.FileSystem{},
+			expected: efs.FileSystem{
+				Metadata:  types.NewTestMetadata(),
+				Encrypted: types.Bool(true, types.NewTestMetadata()),
+			},
+		},
+		{
+			name: "defaults",
+			terraform: `
+			resource "aws_efs_file_system" "example" {
+			  }
+`,
+			expected: efs.FileSystem{
+				Metadata:  types.NewTestMetadata(),
+				Encrypted: types.Bool(false, types.NewTestMetadata()),
+			},
 		},
 	}
 
@@ -87,4 +51,25 @@ resource "" "example" {
 			testutil.AssertDefsecEqual(t, test.expected, adapted)
 		})
 	}
+}
+
+func TestLines(t *testing.T) {
+	src := `
+	resource "aws_efs_file_system" "example" {
+		name       = "bar"
+		encrypted  = true
+		kms_key_id = "my_kms_key"
+	  }
+	`
+	modules := testutil.CreateModulesFromSource(src, ".tf", t)
+	adapted := Adapt(modules)
+
+	require.Len(t, adapted.FileSystems, 1)
+	fileSystem := adapted.FileSystems[0]
+
+	assert.Equal(t, 2, fileSystem.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 6, fileSystem.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 4, fileSystem.Encrypted.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 4, fileSystem.Encrypted.GetMetadata().Range().GetEndLine())
 }
