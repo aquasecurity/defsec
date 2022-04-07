9a4a74f3db8f3e99c9f078785e9adee58609daa7
Liam Galvin <liam.galvin@aquasec.com>
2022-04-07 19:20:17 +0100

    Update scanner.go

--- pkg/scanners/terraform/scanner.go
+++ pkg/scanners/terraform/scanner.go
@@ -165,7 +165,7 @@ func (s *Scanner) ScanFSWithMetrics(ctx context.Context, target fs.FS, dir strin
 				override := scan.Results{
 					result,
 				}
-				override.SetSourceAndFilesystem(result.GetSourcePrefix(), filesystem)
+				override.SetSourceAndFilesystem(result.Range().GetSourcePrefix(), filesystem)
 				results[i] = override[0]
 			}
 		}
