eaf83c5cffebc1e94bde9a68951ff4db550ef1d3
Vanesa Sejdiu <vsejdiu4@gmail.com>
2022-04-23 17:03:02 +0200

    test: add tests for aws redshift tf adapters

--- internal/adapters/terraform/aws/redshift/adapt_test.go
+++ internal/adapters/terraform/aws/redshift/adapt_test.go
@@ -1,30 +1,63 @@
 package redshift
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/aquasecurity/defsec/pkg/providers/aws/redshift"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/aquasecurity/defsec/internal/adapters/terraform/tftestutil"
+	"github.com/aquasecurity/defsec/internal/types"
 
 	"github.com/aquasecurity/defsec/test/testutil"
 )
 
 func Test_Adapt(t *testing.T) {
-	t.SkipNow()
 	tests := []struct {
 		name      string
 		terraform string
 		expected  redshift.Redshift
 	}{
 		{
-			name: "basic",
+			name: "reference key id",
 			terraform: `
-resource "" "example" {
-    
-}
+			resource "aws_kms_key" "redshift" {
+				enable_key_rotation = true
+			}
+			
+			resource "aws_redshift_cluster" "example" {
+			  cluster_identifier = "tf-redshift-cluster"
+			  encrypted          = true
+			  kms_key_id         = aws_kms_key.redshift.key_id
+			  cluster_subnet_group_name = "redshift_subnet"
+			}
+
+			resource "aws_redshift_security_group" "default" {
+				name = "redshift-sg"
+				description = "some description"
+			}
 `,
-			expected: redshift.Redshift{},
+			expected: redshift.Redshift{
+				Clusters: []redshift.Cluster{
+					{
+						Metadata: types.NewTestMetadata(),
+						Encryption: redshift.Encryption{
+							Metadata: types.NewTestMetadata(),
+							Enabled:  types.Bool(true, types.NewTestMetadata()),
+							KMSKeyID: types.String("aws_kms_key.redshift", types.NewTestMetadata()),
+						},
+						SubnetGroupName: types.String("redshift_subnet", types.NewTestMetadata()),
+					},
+				},
+				SecurityGroups: []redshift.SecurityGroup{
+					{
+						Metadata:    types.NewTestMetadata(),
+						Description: types.String("some description", types.NewTestMetadata()),
+					},
+				},
+			},
 		},
 	}
 
@@ -32,107 +65,81 @@ resource "" "example" {
 		t.Run(test.name, func(t *testing.T) {
 			modules := tftestutil.CreateModulesFromSource(t, test.terraform, ".tf")
 			adapted := Adapt(modules)
-			testutil.AssertDefsecEqual(t, test.expected, adapted)
-		})
-	}
-}
-
-func Test_adaptClusters(t *testing.T) {
-	t.SkipNow()
-	tests := []struct {
-		name      string
-		terraform string
-		expected  []redshift.Cluster
-	}{
-		{
-			name: "basic",
-			terraform: `
-resource "" "example" {
-    
-}
-`,
-			expected: []redshift.Cluster{},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			modules := tftestutil.CreateModulesFromSource(t, test.terraform, ".tf")
-			adapted := adaptClusters(modules)
-			testutil.AssertDefsecEqual(t, test.expected, adapted)
-		})
-	}
-}
-
-func Test_adaptSecurityGroups(t *testing.T) {
-	t.SkipNow()
-	tests := []struct {
-		name      string
-		terraform string
-		expected  []redshift.SecurityGroup
-	}{
-		{
-			name: "basic",
-			terraform: `
-resource "" "example" {
-    
-}
-`,
-			expected: []redshift.SecurityGroup{},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			modules := tftestutil.CreateModulesFromSource(t, test.terraform, ".tf")
-			adapted := adaptSecurityGroups(modules)
+			fmt.Println(adapted.SecurityGroups[0].Description.Value())
 			testutil.AssertDefsecEqual(t, test.expected, adapted)
 		})
 	}
 }
 
 func Test_adaptCluster(t *testing.T) {
-	t.SkipNow()
 	tests := []struct {
 		name      string
 		terraform string
 		expected  redshift.Cluster
 	}{
 		{
-			name: "basic",
-			terraform: `
-resource "" "example" {
-    
-}
+			name: "key as string",
+			terraform: `			
+			resource "aws_redshift_cluster" "example" {
+			  cluster_identifier = "tf-redshift-cluster"
+			  encrypted          = true
+			  kms_key_id         = "key-id"
+			  cluster_subnet_group_name = "redshift_subnet"
+			}
 `,
-			expected: redshift.Cluster{},
+			expected: redshift.Cluster{
+				Metadata: types.NewTestMetadata(),
+				Encryption: redshift.Encryption{
+					Metadata: types.NewTestMetadata(),
+					Enabled:  types.Bool(true, types.NewTestMetadata()),
+					KMSKeyID: types.String("key-id", types.NewTestMetadata()),
+				},
+				SubnetGroupName: types.String("redshift_subnet", types.NewTestMetadata()),
+			},
+		},
+		{
+			name: "defaults",
+			terraform: `			
+			resource "aws_redshift_cluster" "example" {
+			}
+`,
+			expected: redshift.Cluster{
+				Metadata: types.NewTestMetadata(),
+				Encryption: redshift.Encryption{
+					Metadata: types.NewTestMetadata(),
+					Enabled:  types.Bool(false, types.NewTestMetadata()),
+					KMSKeyID: types.String("", types.NewTestMetadata()),
+				},
+				SubnetGroupName: types.String("", types.NewTestMetadata()),
+			},
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			modules := tftestutil.CreateModulesFromSource(t, test.terraform, ".tf")
-			adapted := adaptCluster(modules.GetBlocks()[0])
+			adapted := adaptCluster(modules.GetBlocks()[0], modules[0])
 			testutil.AssertDefsecEqual(t, test.expected, adapted)
 		})
 	}
 }
 
 func Test_adaptSecurityGroup(t *testing.T) {
-	t.SkipNow()
 	tests := []struct {
 		name      string
 		terraform string
 		expected  redshift.SecurityGroup
 	}{
 		{
-			name: "basic",
+			name: "defaults",
 			terraform: `
 resource "" "example" {
-    
 }
 `,
-			expected: redshift.SecurityGroup{},
+			expected: redshift.SecurityGroup{
+				Metadata:    types.NewTestMetadata(),
+				Description: types.String("Managed by Terraform", types.NewTestMetadata()),
+			},
 		},
 	}
 
@@ -143,4 +150,49 @@ resource "" "example" {
 			testutil.AssertDefsecEqual(t, test.expected, adapted)
 		})
 	}
+}
+
+func TestLines(t *testing.T) {
+	src := `
+	resource "aws_kms_key" "redshift" {
+		enable_key_rotation = true
+	}
+	
+	resource "aws_redshift_cluster" "example" {
+	  cluster_identifier = "tf-redshift-cluster"
+	  encrypted          = true
+	  kms_key_id         = aws_kms_key.redshift.key_id
+	  cluster_subnet_group_name = "subnet name"
+	}
+
+	resource "aws_redshift_security_group" "default" {
+		name = "redshift-sg"
+		description = "some description"
+	}`
+
+	modules := tftestutil.CreateModulesFromSource(t, src, ".tf")
+	adapted := Adapt(modules)
+
+	require.Len(t, adapted.Clusters, 1)
+	require.Len(t, adapted.SecurityGroups, 1)
+	cluster := adapted.Clusters[0]
+	securityGroup := adapted.SecurityGroups[0]
+
+	assert.Equal(t, 6, cluster.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 11, cluster.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 8, cluster.Encryption.Enabled.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 8, cluster.Encryption.Enabled.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 2, cluster.Encryption.KMSKeyID.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 4, cluster.Encryption.KMSKeyID.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 10, cluster.SubnetGroupName.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 10, cluster.SubnetGroupName.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 13, securityGroup.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 16, securityGroup.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 15, securityGroup.Description.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 15, securityGroup.Description.GetMetadata().Range().GetEndLine())
 }
