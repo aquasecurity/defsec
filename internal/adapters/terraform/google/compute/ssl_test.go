dffc03db57485f37e192f7a60ed69022d4d29e11
Vanesa Sejdiu <vsejdiu4@gmail.com>
2022-05-22 12:36:41 +0200

    test: add tests for google compute tf adapters

--- internal/adapters/terraform/google/compute/ssl_test.go
+++ internal/adapters/terraform/google/compute/ssl_test.go
@@ -6,25 +6,49 @@ import (
 	"github.com/aquasecurity/defsec/pkg/providers/google/compute"
 
 	"github.com/aquasecurity/defsec/internal/adapters/terraform/tftestutil"
+	"github.com/aquasecurity/defsec/internal/types"
 
 	"github.com/aquasecurity/defsec/test/testutil"
 )
 
 func Test_adaptSSLPolicies(t *testing.T) {
-	t.SkipNow()
 	tests := []struct {
 		name      string
 		terraform string
 		expected  []compute.SSLPolicy
 	}{
 		{
-			name: "basic",
+			name: "defined",
 			terraform: `
-resource "" "example" {
-    
-}
+			resource "google_compute_ssl_policy" "example" {
+				name    = "production-ssl-policy"
+				profile = "MODERN"
+				min_tls_version = "TLS_1_2"
+			  }
 `,
-			expected: []compute.SSLPolicy{},
+			expected: []compute.SSLPolicy{
+				{
+					Metadata:          types.NewTestMetadata(),
+					Name:              types.String("production-ssl-policy", types.NewTestMetadata()),
+					Profile:           types.String("MODERN", types.NewTestMetadata()),
+					MinimumTLSVersion: types.String("TLS_1_2", types.NewTestMetadata()),
+				},
+			},
+		},
+		{
+			name: "defaults",
+			terraform: `
+			resource "google_compute_ssl_policy" "example" {
+			  }
+`,
+			expected: []compute.SSLPolicy{
+				{
+					Metadata:          types.NewTestMetadata(),
+					Name:              types.String("", types.NewTestMetadata()),
+					Profile:           types.String("", types.NewTestMetadata()),
+					MinimumTLSVersion: types.String("TLS_1_0", types.NewTestMetadata()),
+				},
+			},
 		},
 	}
 
