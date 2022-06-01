20132dce475e57d67be4d55162f8a86e69a42d2a
Liam Galvin <liam.galvin@aquasec.com>
2022-06-01 09:29:49 +0100

    fix(cloudformation): Fix unresolved values being equated with empty ones
    Resolves #613
    
    Signed-off-by: Liam Galvin <liam.galvin@aquasec.com>

--- internal/rules/aws/s3/enable_bucket_logging.cf.go
+++ internal/rules/aws/s3/enable_bucket_logging.cf.go
@@ -10,6 +10,24 @@ Resources:
         LogFilePrefix: accesslogs/
     Type: AWS::S3::Bucket
 `,
+	`---
+Resources:
+  MyS3Bucket:
+    Type: AWS::S3::Bucket
+    DeletionPolicy: Retain
+    UpdateReplacePolicy: Retain
+    Properties:
+      BucketName: !Sub my-s3-bucket-${BucketSuffix}
+      LoggingConfiguration:
+        DestinationBucketName: !FindInMap [EnvironmentMapping, s3, logging]
+        LogFilePrefix: !Sub s3-logs/AWSLogs/${AWS::AccountId}/my-s3-bucket-${BucketSuffix}
+      AccessControl: Private
+      PublicAccessBlockConfiguration:
+        BlockPublicAcls: true
+        BlockPublicPolicy: true
+        IgnorePublicAcls: true
+        RestrictPublicBuckets: true
+`,
 }
 
 var cloudFormationEnableBucketLoggingBadExamples = []string{
