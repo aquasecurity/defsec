a4169a0dd2cc2663a1fcd3a8a246bb6847f375a1
Owen Rumney <owen.rumney@aquasec.com>
2022-06-09 14:33:06 +0100

    fix: Add support for missing types in Kubernetes yaml (#689)
    * fix: Add support for missing types in Kubernetes yaml
    * fix: linter issues
    
    Signed-off-by: Owen Rumney <owen.rumney@aquasec.com>

--- pkg/scanners/kubernetes/parser/manifest_node.go
+++ pkg/scanners/kubernetes/parser/manifest_node.go
@@ -12,7 +12,9 @@ type TagType string
 const (
 	TagBool   TagType = "!!bool"
 	TagInt    TagType = "!!int"
-	TagString TagType = "!!str"
+	TagFloat  TagType = "!!float"
+	TagStr    TagType = "!!str"
+	TagString TagType = "!!string"
 	TagSlice  TagType = "!!seq"
 	TagMap    TagType = "!!map"
 )
@@ -30,7 +32,7 @@ func (r *ManifestNode) ToRego() interface{} {
 		return nil
 	}
 	switch r.Type {
-	case TagBool, TagInt, TagString:
+	case TagBool, TagInt, TagString, TagStr:
 		return r.Value
 	case TagSlice:
 		var output []interface{}
@@ -60,10 +62,17 @@ func (r *ManifestNode) UnmarshalYAML(node *yaml.Node) error {
 	r.Type = TagType(node.Tag)
 
 	switch TagType(node.Tag) {
-	case TagString:
+	case TagString, TagStr:
+
 		r.Value = node.Value
 	case TagInt:
 		val, err := strconv.Atoi(node.Value)
+		if err != nil {
+			return err
+		}
+		r.Value = val
+	case TagFloat:
+		val, err := strconv.ParseFloat(node.Value, 64)
 		if err != nil {
 			return err
 		}
@@ -75,44 +84,55 @@ func (r *ManifestNode) UnmarshalYAML(node *yaml.Node) error {
 		}
 		r.Value = val
 	case TagMap:
-		output := make(map[string]ManifestNode)
-		var key string
-		max := node.Line
-		for i, contentNode := range node.Content {
-			if i == 0 || i%2 == 0 {
-				key = contentNode.Value
-			} else {
-				newNode := new(ManifestNode)
-				newNode.Path = r.Path
-				if err := contentNode.Decode(newNode); err != nil {
-					return err
-				}
-				output[key] = *newNode
-				if newNode.EndLine > max {
-					max = newNode.EndLine
-				}
-			}
-		}
-		r.EndLine = max
-		r.Value = output
+		return r.handleMapTag(node)
 	case TagSlice:
-		var nodes []ManifestNode
-		max := node.Line
-		for _, contentNode := range node.Content {
+		return r.handleSliceTag(node)
+
+	default:
+		return fmt.Errorf("node tag is not supported %s", node.Tag)
+	}
+	return nil
+}
+
+func (r *ManifestNode) handleSliceTag(node *yaml.Node) error {
+	var nodes []ManifestNode
+	max := node.Line
+	for _, contentNode := range node.Content {
+		newNode := new(ManifestNode)
+		newNode.Path = r.Path
+		if err := contentNode.Decode(newNode); err != nil {
+			return err
+		}
+		if newNode.EndLine > max {
+			max = newNode.EndLine
+		}
+		nodes = append(nodes, *newNode)
+	}
+	r.EndLine = max
+	r.Value = nodes
+	return nil
+}
+
+func (r *ManifestNode) handleMapTag(node *yaml.Node) error {
+	output := make(map[string]ManifestNode)
+	var key string
+	max := node.Line
+	for i, contentNode := range node.Content {
+		if i == 0 || i%2 == 0 {
+			key = contentNode.Value
+		} else {
 			newNode := new(ManifestNode)
 			newNode.Path = r.Path
 			if err := contentNode.Decode(newNode); err != nil {
 				return err
 			}
+			output[key] = *newNode
 			if newNode.EndLine > max {
 				max = newNode.EndLine
 			}
-			nodes = append(nodes, *newNode)
 		}
-		r.EndLine = max
-		r.Value = nodes
-	default:
-		return fmt.Errorf("node tag is not supported %s", node.Tag)
 	}
+	r.EndLine = max
+	r.Value = output
 	return nil
 }
