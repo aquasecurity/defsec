f3738954f0eb44a20eef029e044547e2c0ec2fcc
Liam Galvin <liam.galvin@aquasec.com>
2022-07-07 12:47:24 +0100

    feat: Add IAM adapter

--- internal/adapters/cloud/aws_services.go
+++ internal/adapters/cloud/aws_services.go
@@ -1,8 +1,8 @@
 package cloud
 
 import (
-	//_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/ec2"
+	_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/ec2"
 	_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/iam"
-	//_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/s3"
-	//_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/sqs"
+	_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/s3"
+	_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/sqs"
 )
