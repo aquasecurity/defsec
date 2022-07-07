bcfdf3c0fc46a62a96305ed70facd4d929c231c7
Liam Galvin <liam.galvin@aquasec.com>
2022-07-07 13:56:02 +0100

    add sns

--- internal/adapters/cloud/aws_services.go
+++ internal/adapters/cloud/aws_services.go
@@ -4,5 +4,6 @@ import (
 	_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/ec2"
 	_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/iam"
 	_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/s3"
+	_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/sns"
 	_ "github.com/aquasecurity/defsec/internal/adapters/cloud/aws/sqs"
 )
