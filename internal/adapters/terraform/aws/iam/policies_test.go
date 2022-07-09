d480aaef1d13217286abe63cbdad18350fe9f955
Vanesa Sejdiu <vsejdiu4@gmail.com>
2022-07-09 23:55:26 +0200

    test: add tests for aws iam tf adapters

--- internal/adapters/terraform/aws/iam/policies_test.go
+++ internal/adapters/terraform/aws/iam/policies_test.go
@@ -4,14 +4,15 @@ import (
 	"testing"
 
 	"github.com/aquasecurity/defsec/pkg/providers/aws/iam"
+	"github.com/liamg/iamgo"
 
 	"github.com/aquasecurity/defsec/internal/adapters/terraform/tftestutil"
+	"github.com/aquasecurity/defsec/internal/types"
 
 	"github.com/aquasecurity/defsec/test/testutil"
 )
 
 func Test_adaptPolicies(t *testing.T) {
-	t.SkipNow()
 	tests := []struct {
 		name      string
 		terraform string
@@ -20,11 +21,49 @@ func Test_adaptPolicies(t *testing.T) {
 		{
 			name: "basic",
 			terraform: `
-resource "" "example" {
-    
-}
+			resource "aws_iam_policy" "policy" {
+				name = "test"	
+
+				policy = jsonencode({
+					Version = "2012-10-17"
+					Statement = [
+					  {
+						Action = [
+						  "ec2:Describe*",
+						]
+						Effect   = "Allow"
+						Resource = "*"
+					  },
+					]
+				  })
+			  }
 `,
-			expected: []iam.Policy{},
+			expected: []iam.Policy{
+				{
+					Metadata: types.NewTestMetadata(),
+					Name:     types.String("test", types.NewTestMetadata()),
+					Document: func() iam.Document {
+
+						builder := iamgo.NewPolicyBuilder()
+						builder.WithVersion("2012-10-17")
+
+						sb := iamgo.NewStatementBuilder()
+
+						sb.WithEffect(iamgo.EffectAllow)
+						sb.WithActions([]string{"ec2:Describe*"})
+						sb.WithResources([]string{"*"})
+
+						builder.WithStatement(sb.Build())
+
+						return iam.Document{
+							Parsed:   builder.Build(),
+							Metadata: types.NewTestMetadata(),
+							IsOffset: false,
+							HasRefs:  false,
+						}
+					}(),
+				},
+			},
 		},
 	}
 
