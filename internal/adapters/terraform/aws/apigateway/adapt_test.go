bccce03f3fc77ecad27acddfd055672f11784a4f
Vanesa Sejdiu <vsejdiu4@gmail.com>
2022-07-10 00:58:06 +0200

    test: add tests for aws apigateway tf adapters metadata

--- internal/adapters/terraform/aws/apigateway/adapt_test.go
+++ internal/adapters/terraform/aws/apigateway/adapt_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/aquasecurity/defsec/internal/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/aquasecurity/defsec/pkg/providers/aws/apigateway"
 
@@ -124,4 +126,96 @@ func Bool(b bool) types.BoolValue {
 
 func String(s string) types.StringValue {
 	return types.String(s, types.NewTestMetadata())
+}
+func TestLines(t *testing.T) {
+	src := `
+	resource "aws_api_gateway_rest_api" "MyDemoAPI" {
+		name        = "MyDemoAPI"
+		description = "This is my API for demonstration purposes"
+	  }
+	  
+	  resource "aws_api_gateway_method" "example" {
+		  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+		  http_method      = "GET"
+		  authorization    = "NONE"
+		  api_key_required = true
+	  }
+
+	  resource "aws_apigatewayv2_api" "example" {
+		  name = "tfsec"
+		  protocol_type = "HTTP"
+	  }
+	  
+	  resource "aws_apigatewayv2_stage" "example" {
+		  api_id = aws_apigatewayv2_api.example.id
+		  name = "tfsec" 
+		  access_log_settings {
+			  destination_arn = "arn:123"
+		  }
+	  }
+	  
+	  resource "aws_api_gateway_domain_name" "example" {
+		  domain_name = "v1.com"
+		  security_policy = "TLS_1_0"
+	  }
+
+	`
+	modules := tftestutil.CreateModulesFromSource(t, src, ".tf")
+	adapted := Adapt(modules)
+
+	require.Len(t, adapted.APIs, 2)
+	require.Len(t, adapted.DomainNames, 1)
+
+	apiV1 := adapted.APIs[0]
+	apiV2 := adapted.APIs[1]
+	domainName := adapted.DomainNames[0]
+
+	assert.Equal(t, 2, apiV1.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 5, apiV1.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 3, apiV1.Name.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 3, apiV1.Name.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 7, apiV1.RESTMethods[0].GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 12, apiV1.RESTMethods[0].GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 9, apiV1.RESTMethods[0].HTTPMethod.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 9, apiV1.RESTMethods[0].HTTPMethod.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 10, apiV1.RESTMethods[0].AuthorizationType.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 10, apiV1.RESTMethods[0].AuthorizationType.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 11, apiV1.RESTMethods[0].APIKeyRequired.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 11, apiV1.RESTMethods[0].APIKeyRequired.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 14, apiV2.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 17, apiV2.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 15, apiV2.Name.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 15, apiV2.Name.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 16, apiV2.ProtocolType.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 16, apiV2.ProtocolType.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 19, apiV2.Stages[0].GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 25, apiV2.Stages[0].GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 21, apiV2.Stages[0].Name.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 21, apiV2.Stages[0].Name.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 22, apiV2.Stages[0].AccessLogging.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 24, apiV2.Stages[0].AccessLogging.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 23, apiV2.Stages[0].AccessLogging.CloudwatchLogGroupARN.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 23, apiV2.Stages[0].AccessLogging.CloudwatchLogGroupARN.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 27, domainName.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 30, domainName.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 28, domainName.Name.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 28, domainName.Name.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 29, domainName.SecurityPolicy.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 29, domainName.SecurityPolicy.GetMetadata().Range().GetEndLine())
+
 }
