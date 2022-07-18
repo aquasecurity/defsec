5cbe798e35897ad8c2fabdb01dded36d5bc2a46c
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-07-18 10:34:41 +0200

    test: add tests for aws iam tf adapters (#726)

--- internal/adapters/terraform/aws/iam/adapt_test.go
+++ internal/adapters/terraform/aws/iam/adapt_test.go
@@ -3,36 +3,126 @@ package iam
 import (
 	"testing"
 
-	"github.com/aquasecurity/defsec/pkg/providers/aws/iam"
-
 	"github.com/aquasecurity/defsec/internal/adapters/terraform/tftestutil"
-
-	"github.com/aquasecurity/defsec/test/testutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func Test_Adapt(t *testing.T) {
-	t.SkipNow()
-	tests := []struct {
-		name      string
-		terraform string
-		expected  iam.IAM
-	}{
-		{
-			name: "basic",
-			terraform: `
-resource "" "example" {
-    
-}
-`,
-			expected: iam.IAM{},
-		},
-	}
+func TestLines(t *testing.T) {
+	src := `
+	resource "aws_iam_account_password_policy" "strict" {
+		minimum_password_length        = 8
+		require_lowercase_characters   = true
+		require_numbers                = true
+		require_uppercase_characters   = true
+		require_symbols                = true
+	  }
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			modules := tftestutil.CreateModulesFromSource(t, test.terraform, ".tf")
-			adapted := Adapt(modules)
-			testutil.AssertDefsecEqual(t, test.expected, adapted)
-		})
-	}
+	resource "aws_iam_group" "my_developers" {
+		name = "developers"
+		path = "/users/"
+	  }
+
+	  resource "aws_iam_group_policy" "my_developer_policy" {
+		name  = "my_developer_policy"
+		group = aws_iam_group.my_developers.name
+
+		policy = <<EOF
+		{
+		  "Version": "2012-10-17",
+		  "Statement": [
+		  {
+			"Sid": "new policy",
+			"Effect": "Allow",
+			"Resource": "*",
+			"Action": [
+				"ec2:Describe*"
+			]
+		  }
+		  ]
+		}
+		EOF
+	  }
+
+	  resource "aws_iam_user" "lb" {
+		name = "loadbalancer"
+		path = "/system/"
+	  }
+
+	  resource "aws_iam_user_policy" "policy" {
+		name = "test"
+		user = aws_iam_user.lb.name
+
+
+		policy = jsonencode({
+			Version = "2012-10-17"
+			Statement = [
+			  {
+				Action = [
+				  "ec2:Describe*",
+				]
+				Effect   = "Allow"
+				Resource = "*"
+			  },
+			]
+		  })
+	  }
+	`
+	modules := tftestutil.CreateModulesFromSource(t, src, ".tf")
+	adapted := Adapt(modules)
+
+	require.Len(t, adapted.Groups, 1)
+	require.Len(t, adapted.Users, 1)
+
+	group := adapted.Groups[0]
+	user := adapted.Users[0]
+	policy := adapted.PasswordPolicy
+
+	assert.Equal(t, 2, policy.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 8, policy.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 3, policy.MinimumLength.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 3, policy.MinimumLength.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 4, policy.RequireLowercase.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 4, policy.RequireLowercase.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 5, policy.RequireNumbers.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 5, policy.RequireNumbers.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 6, policy.RequireUppercase.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 6, policy.RequireUppercase.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 7, policy.RequireSymbols.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 7, policy.RequireSymbols.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 10, group.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 13, group.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 11, group.Name.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 11, group.Name.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 15, group.Policies[0].GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 34, group.Policies[0].GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 16, group.Policies[0].Name.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 16, group.Policies[0].Name.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 19, group.Policies[0].Document.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 33, group.Policies[0].Document.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 36, user.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 39, user.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 37, user.Name.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 37, user.Name.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 41, user.Policies[0].GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 58, user.Policies[0].GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 42, user.Policies[0].Name.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 42, user.Policies[0].Name.GetMetadata().Range().GetEndLine())
+
+	assert.Equal(t, 46, user.Policies[0].Document.GetMetadata().Range().GetStartLine())
+	assert.Equal(t, 57, user.Policies[0].Document.GetMetadata().Range().GetEndLine())
 }
