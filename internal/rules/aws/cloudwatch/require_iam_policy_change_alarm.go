4ad2a61eb392136b8f3af6c27f5b3c2550bdcfdf
Owen Rumney <owen.rumney@aquasec.com>
2022-08-03 09:06:44 +0100

    feat: add check for IAM policy change alarm (#786)
    Signed-off-by: Owen Rumney <owen.rumney@aquasec.com>

--- internal/rules/aws/cloudwatch/require_iam_policy_change_alarm.go
+++ internal/rules/aws/cloudwatch/require_iam_policy_change_alarm.go
@@ -11,26 +11,26 @@ import (
 	"github.com/aquasecurity/defsec/pkg/state"
 )
 
-var requireNonMFALoginAlarm = rules.Register(
+var requireIAMPolicyChangeAlarm = rules.Register(
 	scan.Rule{
-		AVDID:      "AVD-AWS-0148",
+		AVDID:      "AVD-AWS-0150",
 		Provider:   providers.AWSProvider,
 		Service:    "cloudwatch",
-		ShortCode:  "require-non-mfa-login-alarm",
-		Summary:    "Ensure a log metric filter and alarm exist for AWS Management Console sign-in without MFA",
-		Impact:     "Not alerting on logins with no MFA allows the risk to go un-notified.",
-		Resolution: "Create an alarm to alert on non MFA logins",
+		ShortCode:  "require-iam-policy-change-alarm",
+		Summary:    "Ensure a log metric filter and alarm exist for IAM policy changes",
+		Impact:     "IAM Policy changes could lead to excessive permissions and may have been performed maliciously.",
+		Resolution: "Create an alarm to alert on IAM Policy changes",
 		Frameworks: map[framework.Framework][]string{
 			framework.CIS_AWS_1_2: {
-				"3.2",
+				"4.4",
 			},
 			framework.CIS_AWS_1_4: {
-				"4.2",
+				"4.4",
 			},
 		},
-		Explanation: `You can do real-time monitoring of API calls by directing CloudTrail logs to CloudWatch Logs and establishing corresponding metric filters and alarms.   
+		Explanation: `  You can do real-time monitoring of API calls by directing CloudTrail logs to CloudWatch Logs and establishing corresponding metric filters and alarms.   
                                                                               
-  CIS recommends that you create a metric filter and alarm console logins that  aren't protected by MFA. Monitoring for single-factor console logins increases visibility into accounts that aren't protected by MFA.`,
+CIS recommends that you create a metric filter and alarm for changes made to IAM policies. Monitoring these changes helps ensure that authentication and authorization controls remain intact.`,
 		Links: []string{
 			"https://aws.amazon.com/iam/features/mfa/",
 		},
@@ -50,10 +50,22 @@ var requireNonMFALoginAlarm = rules.Register(
 			var metricFilter cloudwatch.MetricFilter
 			var found bool
 			for _, filter := range logGroup.MetricFilters {
-				if filter.FilterPattern.Contains(`($.eventName = "ConsoleLogin") && 
-($.additionalEventData.MFAUsed != "Yes") && 
-($.userIdentity.type=="IAMUser") && 
-($.responseElements.ConsoleLogin == "Success")`, types.IgnoreWhitespace) {
+				if filter.FilterPattern.Contains(`{($.eventName=DeleteGroupPolicy) || 
+($.eventName=DeleteRolePolicy) || 
+($.eventName=DeleteUserPolicy) || 
+($.eventName=PutGroupPolicy) || 
+($.eventName=PutRolePolicy) || 
+($.eventName=PutUserPolicy) || 
+($.eventName=CreatePolicy) || 
+($.eventName=DeletePolicy) || 
+($.eventName=CreatePolicyVersion) || 
+($.eventName=DeletePolicyVersion) || 
+($.eventName=AttachRolePolicy) ||
+($.eventName=DetachRolePolicy) ||
+($.eventName=AttachUserPolicy) || 
+($.eventName=DetachUserPolicy) || 
+($.eventName=AttachGroupPolicy) || 
+($.eventName=DetachGroupPolicy)}`, types.IgnoreWhitespace) {
 					metricFilter = filter
 					found = true
 					break
@@ -61,12 +73,12 @@ var requireNonMFALoginAlarm = rules.Register(
 			}
 
 			if !found {
-				results.Add("Cloudtrail has no non-MFA login log filter", trail)
+				results.Add("Cloudtrail has IAM policy change log filter", trail)
 				continue
 			}
 
 			if metricAlarm := s.AWS.CloudWatch.GetAlarmByMetricName(metricFilter.FilterName.Value()); metricAlarm == nil {
-				results.Add("Cloudtrail has no non-MFA login alarm", trail)
+				results.Add("Cloudtrail has no IAM Policy change alarm", trail)
 				continue
 			}
 
