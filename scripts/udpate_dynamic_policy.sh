3f2c47f12e8906c23a895b90861d8a4c40796410
chenk <hen.keinan@gmail.com>
2022-09-13 14:32:22 +0300

    chore: cronjob for updating outdated-api policy
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- scripts/udpate_dynamic_policy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DYNAMIC_REGO_FOLDER=./internal/rules/kubernetes/policies/dynamic
-sed -i.bak "s|{}|$1|" $DYNAMIC_REGO_FOLDER/outdated_api.rego && rm $DYNAMIC_REGO_FOLDER/outdated_api.rego.bak
