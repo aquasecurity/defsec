1e2050265e87c684872b1e81268a253becf48318
Liam Galvin <liam.galvin@aquasec.com>
2022-10-19 14:35:15 +0100

    fix: Fix release tagging of bundle artifact

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -5,6 +5,11 @@ rm bundle.tar.gz || true
 RELEASE_VERSION=${GITHUB_REF/refs\/tags\/v/}
 MINOR_VERSION=$(echo ${RELEASE_VERSION} | cut -d. -f1,2)
 MAJOR_VERSION=$(echo ${RELEASE_VERSION} | cut -d. -f1)
+if [ -n "$GITHUB_ENV" ]; then
+  echo "RELEASE_VERSION=$RELEASE_VERSION" >> $GITHUB_ENV
+  echo "MINOR_VERSION=$MINOR_VERSION" >> $GITHUB_ENV
+  echo "MAJOR_VERSION=$MAJOR_VERSION" >> $GITHUB_ENV
+fi
 mkdir -p bundle/policies
 rsync -avr --exclude=README.md --exclude="*_test.rego" --exclude="*.go" --exclude=test --exclude=advanced internal/rules/policies/ bundle/policies/
 cp internal/rules/policies/.manifest bundle/
