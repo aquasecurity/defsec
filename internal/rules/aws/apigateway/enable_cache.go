394a31a7540dc7b751fb7c9f31c967dce5ef14e3
sanaayousaf <sanayousaf012@gmail.com>
2022-11-10 09:58:05 +0000

    Added_impact

--- internal/rules/aws/apigateway/enable_cache.go
+++ internal/rules/aws/apigateway/enable_cache.go
@@ -15,7 +15,7 @@ var CheckEnableCache = rules.Register(
 		Service:    "api-gateway",
 		ShortCode:  "enable-cache",
 		Summary:    "Ensure that response caching is enabled for your Amazon API Gateway REST APIs.",
-		Impact:     "",
+		Impact:     "Reduce the number of calls made to your API endpoint and also improve the latency of requests to your API with response caching.",
 		Resolution: "Enable cache",
 		Explanation: "A REST API in API Gateway is a collection of resources and methods that are integrated with backend HTTP endpoints, Lambda functions, or other AWS services.You can enable API caching in Amazon API Gateway to cache your endpoint responses." +
 			"With caching, you can reduce the number of calls made to your endpoint and also improve the latency of requests to your API.",
