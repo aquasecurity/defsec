633b9f89bb718bce266ff13b343e3d326689ff04
fuku <fukuzawa.makoto@fjct.fujitsu.com>
2022-12-15 11:35:13 +0900

    feat(nifcloud): add sslcertificate terraform adapter

--- internal/adapters/terraform/nifcloud/nifcloud.go
+++ internal/adapters/terraform/nifcloud/nifcloud.go
@@ -2,12 +2,14 @@ package nifcloud
 
 import (
 	"github.com/aquasecurity/defsec/internal/adapters/terraform/nifcloud/computing"
+	"github.com/aquasecurity/defsec/internal/adapters/terraform/nifcloud/sslcertificate"
 	"github.com/aquasecurity/defsec/pkg/providers/nifcloud"
 	"github.com/aquasecurity/defsec/pkg/terraform"
 )
 
 func Adapt(modules terraform.Modules) nifcloud.Nifcloud {
 	return nifcloud.Nifcloud{
-		Computing: computing.Adapt(modules),
+		Computing:      computing.Adapt(modules),
+		SSLCertificate: sslcertificate.Adapt(modules),
 	}
 }
--- pkg/providers/nifcloud/nifcloud.go
+++ pkg/providers/nifcloud/nifcloud.go
@@ -2,8 +2,10 @@ package nifcloud
 
 import (
 	"github.com/aquasecurity/defsec/pkg/providers/nifcloud/computing"
+	"github.com/aquasecurity/defsec/pkg/providers/nifcloud/sslcertificate"
 )
 
 type Nifcloud struct {
-	Computing computing.Computing
+	Computing      computing.Computing
+	SSLCertificate sslcertificate.SSLCertificate
 }
