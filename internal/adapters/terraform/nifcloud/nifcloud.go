ad836f3cc0d4454a4017dcfcce4135fe5421645a
fuku <fukuzawa.makoto@fjct.fujitsu.com>
2022-12-20 14:35:55 +0900

    feat(nifcloud): add rdb terraform adapter

--- internal/adapters/terraform/nifcloud/nifcloud.go
+++ internal/adapters/terraform/nifcloud/nifcloud.go
@@ -3,6 +3,7 @@ package nifcloud
 import (
 	"github.com/aquasecurity/defsec/internal/adapters/terraform/nifcloud/computing"
 	"github.com/aquasecurity/defsec/internal/adapters/terraform/nifcloud/dns"
+	"github.com/aquasecurity/defsec/internal/adapters/terraform/nifcloud/rdb"
 	"github.com/aquasecurity/defsec/internal/adapters/terraform/nifcloud/sslcertificate"
 	"github.com/aquasecurity/defsec/pkg/providers/nifcloud"
 	"github.com/aquasecurity/defsec/pkg/terraform"
@@ -12,6 +13,7 @@ func Adapt(modules terraform.Modules) nifcloud.Nifcloud {
 	return nifcloud.Nifcloud{
 		Computing:      computing.Adapt(modules),
 		DNS:            dns.Adapt(modules),
+		RDB:            rdb.Adapt(modules),
 		SSLCertificate: sslcertificate.Adapt(modules),
 	}
 }
--- pkg/providers/nifcloud/nifcloud.go
+++ pkg/providers/nifcloud/nifcloud.go
@@ -3,11 +3,13 @@ package nifcloud
 import (
 	"github.com/aquasecurity/defsec/pkg/providers/nifcloud/computing"
 	"github.com/aquasecurity/defsec/pkg/providers/nifcloud/dns"
+	"github.com/aquasecurity/defsec/pkg/providers/nifcloud/rdb"
 	"github.com/aquasecurity/defsec/pkg/providers/nifcloud/sslcertificate"
 )
 
 type Nifcloud struct {
 	Computing      computing.Computing
 	DNS            dns.DNS
+	RDB            rdb.RDB
 	SSLCertificate sslcertificate.SSLCertificate
 }
