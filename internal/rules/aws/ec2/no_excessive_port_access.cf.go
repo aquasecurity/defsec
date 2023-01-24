01e314e6861965651612bb9cb75c51afe9ed91b1
Gio Rodriguez <gioroddev@gmail.com>
2023-01-24 18:04:55 -0500

    fix(rules) updated logic for ec2 nacl rule (#1146)
    * updating plugin logic and metadata to be consistent with the check being done.
    Updated cfn/terraform files to have the consistent logic
    
    * updating tests
    
    * changing logic back and only adding 'allow' logic
    
    Co-authored-by: Gio Rodriguez <giovanni.rodriguez@aquasec.com>

--- internal/rules/aws/ec2/no_excessive_port_access.cf.go
+++ internal/rules/aws/ec2/no_excessive_port_access.cf.go
@@ -3,15 +3,17 @@ package ec2
 var cloudFormationNoExcessivePortAccessGoodExamples = []string{
 	`---
 AWSTemplateFormatVersion: 2010-09-09
-Description: Godd example of excessive ports
+Description: Good example of excessive ports
 Resources: 
   NetworkACL:
     Type: AWS::EC2::NetworkAcl
     Properties:
       VpcId: "something"
+	  RuleAction: "allow"
   Rule:
     Type: AWS::EC2::NetworkAclEntry
     Properties:
+      RuleAction: "allow"
       NetworkAclId:
         Ref: NetworkACL
       Protocol: 6
@@ -27,12 +29,14 @@ Resources:
     Type: AWS::EC2::NetworkAcl
     Properties:
       VpcId: "something"
+      RuleAction: "allow"
   Rule:
     Type: AWS::EC2::NetworkAclEntry
     Properties:
       NetworkAclId:
         Ref: NetworkACL
       Protocol: -1
+      RuleAction: "allow"
 `,
 }
 
