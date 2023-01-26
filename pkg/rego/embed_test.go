8a052bd3c034da8a98018b09c635ce533ba15db2
simar7 <1254783+simar7@users.noreply.github.com>
2023-01-26 10:53:59 -0800

    feat(schema): Add ability to provide schema (#1143)
    * poc/schema.input: WIP for initial policy loading
    
    Signed-off-by: Simar <simar@linux.com>
    
    * improve embedded schema placement logic
    
    Signed-off-by: Simar <simar@linux.com>
    
    * add tests
    
    Signed-off-by: Simar <simar@linux.com>
    
    * add more tests for embed logic
    
    Signed-off-by: Simar <simar@linux.com>
    
    * DRY
    
    Signed-off-by: Simar <simar@linux.com>
    
    * detect custom schemas in passed filesystem
    
    Signed-off-by: Simar <simar@linux.com>
    
    * simplify tests
    
    Signed-off-by: Simar <simar@linux.com>
    
    Signed-off-by: Simar <simar@linux.com>
    Co-authored-by: Gio Rodriguez <gioroddev@gmail.com>

--- pkg/rego/embed_test.go
+++ pkg/rego/embed_test.go
@@ -4,8 +4,9 @@ import (
 	"testing"
 
 	"github.com/aquasecurity/defsec/internal/rules"
-
+	"github.com/open-policy-agent/opa/ast"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_EmbeddedLoading(t *testing.T) {
@@ -18,4 +19,104 @@ func Test_EmbeddedLoading(t *testing.T) {
 		}
 	}
 	assert.True(t, found, "no embedded rego policies were registered as rules")
+}
+
+func Test_RegisterRegoRules(t *testing.T) {
+	var testCases = []struct {
+		name          string
+		inputPolicy   string
+		expectedError bool
+	}{
+		{
+			name: "happy path old single schema",
+			inputPolicy: `# METADATA
+# title: "dummy title"
+# description: "some description"
+# scope: package
+# schemas:
+# - input: schema["input"]
+# custom:
+#   input:
+#     selector:
+#     - type: dockerfile
+package builtin.dockerfile.DS1234
+deny[res]{
+	res := true
+}`,
+		},
+		{
+			name: "happy path new builtin single schema",
+			inputPolicy: `# METADATA
+# title: "dummy title"
+# description: "some description"
+# scope: package
+# schemas:
+# - input: schema["dockerfile"]
+# custom:
+#   input:
+#     selector:
+#     - type: dockerfile
+package builtin.dockerfile.DS1234
+deny[res]{
+	res := true
+}`,
+		},
+		{
+			name: "happy path new multiple schemas",
+			inputPolicy: `# METADATA
+# title: "dummy title"
+# description: "some description"
+# scope: package
+# schemas:
+# - input: schema["dockerfile"]
+# - input: schema["kubernetes"]
+# custom:
+#   input:
+#     selector:
+#     - type: dockerfile
+package builtin.dockerfile.DS1234
+deny[res]{
+	res := true
+}`,
+		},
+		{
+			name: "sad path schema does not exist",
+			inputPolicy: `# METADATA
+# title: "dummy title"
+# description: "some description"
+# scope: package
+# schemas:
+# - input: schema["invalid schema"]
+# custom:
+#   input:
+#     selector:
+#     - type: dockerfile
+package builtin.dockerfile.DS1234
+deny[res]{
+	res := true
+}`,
+			expectedError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			policies, err := RecurseEmbeddedModules(rules.EmbeddedLibraryFileSystem, ".")
+			require.NoError(t, err)
+			newRule, err := ast.ParseModuleWithOpts("/rules/newrule.rego", tc.inputPolicy, ast.ParserOptions{
+				ProcessAnnotation: true,
+			})
+			require.NoError(t, err)
+
+			policies["/rules/newrule.rego"] = newRule
+			switch {
+			case tc.expectedError:
+				assert.Panics(t, func() {
+					RegisterRegoRules(policies)
+				}, tc.name)
+			default:
+				RegisterRegoRules(policies)
+			}
+		})
+	}
 }
