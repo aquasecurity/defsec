ac437721883a88575c409e70ff65f0127ed2be55
realwebdev <haseebshalan@gmail.com>
2023-01-30 18:32:19 +0500

    rds policies added

--- pkg/providers/aws/rds/rds.go
+++ pkg/providers/aws/rds/rds.go
@@ -5,9 +5,37 @@ import (
 )
 
 type RDS struct {
-	Instances []Instance
-	Clusters  []Cluster
-	Classic   Classic
+	Instances          []Instance
+	Clusters           []Cluster
+	Classic            Classic
+	Snapshots          []Snapshots
+	Parameters         []Parameters
+	SnapshotAttributes []SnapshotAttributes
+	ParameterGroups    []ParameterGroups
+}
+
+type Instance struct {
+	Metadata                         defsecTypes.Metadata
+	BackupRetentionPeriodDays        defsecTypes.IntValue
+	ReplicationSourceARN             defsecTypes.StringValue
+	PerformanceInsights              PerformanceInsights
+	Encryption                       Encryption
+	PublicAccess                     defsecTypes.BoolValue
+	Engine                           defsecTypes.StringValue
+	IAMAuthEnabled                   defsecTypes.BoolValue
+	DeletionProtection               defsecTypes.BoolValue
+	DBInstanceArn                    defsecTypes.StringValue
+	StorageEncrypted                 defsecTypes.BoolValue
+	DBInstanceIdentifier             defsecTypes.StringValue
+	DBParameterGroups                []DBParameterGroupsList
+	TagList                          []defsecTypes.MapValue
+	EnabledCloudwatchLogsExports     []defsecTypes.StringValue
+	EngineVersion                    defsecTypes.StringValue
+	AutoMinorVersionUpgrade          defsecTypes.BoolValue
+	MultiAZ                          defsecTypes.BoolValue
+	PubliclyAccessible               defsecTypes.BoolValue
+	LatestRestorableTime             defsecTypes.TimeValue
+	ReadReplicaDBInstanceIdentifiers []defsecTypes.StringValue
 }
 
 type Cluster struct {
@@ -19,6 +47,54 @@ type Cluster struct {
 	Encryption                Encryption
 	PublicAccess              defsecTypes.BoolValue
 	Engine                    defsecTypes.StringValue
+	LatestRestorableTime      defsecTypes.TimeValue
+}
+
+type Snapshots struct {
+	Metadata    defsecTypes.Metadata
+	DBSnapshots []DBSnapshots
+}
+
+type DBSnapshots struct {
+	Metadata             defsecTypes.Metadata
+	DBSnapshotIdentifier defsecTypes.StringValue
+	DBSnapshotArn        defsecTypes.StringValue
+	Encrypted            defsecTypes.BoolValue
+}
+
+type Parameters struct {
+	Metadata       defsecTypes.Metadata
+	ParameterName  defsecTypes.StringValue
+	ParameterValue defsecTypes.StringValue
+}
+
+type ParameterGroups struct {
+	Metadata          defsecTypes.Metadata
+	DBParameterGroups []DBParameterGroups
+}
+
+type SnapshotAttributes struct {
+	Metadata                   defsecTypes.Metadata
+	DBSnapshotAttributesResult []DBSnapshotAttributesResult
+	KMSKeyID                   defsecTypes.StringValue
+}
+
+type DBSnapshotAttributesResult struct {
+	Metadata             defsecTypes.Metadata
+	DBSnapshotAttributes []DBSnapshotAttributes
+	KMSKeyID             defsecTypes.StringValue
+}
+
+type DBSnapshotAttributes struct {
+	Metadata        defsecTypes.Metadata
+	AttributeValues []defsecTypes.StringValue
+	KMSKeyID        defsecTypes.StringValue
+}
+
+type DBParameterGroups struct {
+	Metadata               defsecTypes.Metadata
+	DBParameterGroupName   defsecTypes.StringValue
+	DBParameterGroupFamily defsecTypes.StringValue
 }
 
 const (
@@ -48,18 +124,6 @@ type Encryption struct {
 	KMSKeyID       defsecTypes.StringValue
 }
 
-type Instance struct {
-	Metadata                  defsecTypes.Metadata
-	BackupRetentionPeriodDays defsecTypes.IntValue
-	ReplicationSourceARN      defsecTypes.StringValue
-	PerformanceInsights       PerformanceInsights
-	Encryption                Encryption
-	PublicAccess              defsecTypes.BoolValue
-	Engine                    defsecTypes.StringValue
-	IAMAuthEnabled            defsecTypes.BoolValue
-	DeletionProtection        defsecTypes.BoolValue
-}
-
 type ClusterInstance struct {
 	Instance
 	ClusterIdentifier defsecTypes.StringValue
@@ -69,4 +133,10 @@ type PerformanceInsights struct {
 	Metadata defsecTypes.Metadata
 	Enabled  defsecTypes.BoolValue
 	KMSKeyID defsecTypes.StringValue
+}
+
+type DBParameterGroupsList struct {
+	Metadata             defsecTypes.Metadata
+	DBParameterGroupName defsecTypes.StringValue
+	KMSKeyID             defsecTypes.StringValue
 }
