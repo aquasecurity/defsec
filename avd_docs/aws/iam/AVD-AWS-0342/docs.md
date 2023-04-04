5389e229b3274546193fb9598bafe1acea426b75
realwebdev <haseebshalan@gmail.com>
2023-04-04 12:27:52 +0500

    fix

--- avd_docs/aws/iam/AVD-AWS-0342/docs.md
+++ avd_docs/aws/iam/AVD-AWS-0342/docs.md
@@ -1,6 +1,5 @@
 
-
-       When a service that needs to perform other actions is used (user, role, human, code, or service), the AWS architecture frequently has that service assume an AWS role to carry out the other actions, the service carrying out the actions is "provided" a role by the calling principal and implicitly takes on that role to carry out the actions (instead of executing sts:AssumeRole).
+In iam:PassRole the service carrying out the actions is "provided" a role by the calling principal and implicitly takes on that role to carry out the actions (instead of executing sts:AssumeRole).
        The privileges attached to the role are distinct from those of the primary ordering the action and may even be larger and can cause security issues.
            
 
--- rules/cloud/policies/aws/iam/filter_iam_pass_role.go
+++ rules/cloud/policies/aws/iam/filter_iam_pass_role.go
@@ -30,8 +30,7 @@ var FilterIamPassRole = rules.Register(
 		Summary:    "To configure many AWS services, you must pass an IAM role to the service. \nThis allows the service to assume the role later and perform actions on your behalf. \nif there is no check it will escalate privileges.",
 		Impact:     "Compromise on security of aws resources.",
 		Resolution: "Change and in permission of the role and resources.",
-		Explanation: `
-       When a service that needs to perform other actions is used (user, role, human, code, or service), the AWS architecture frequently has that service assume an AWS role to carry out the other actions, the service carrying out the actions is "provided" a role by the calling principal and implicitly takes on that role to carry out the actions (instead of executing sts:AssumeRole).
+		Explanation: `In iam:PassRole the service carrying out the actions is "provided" a role by the calling principal and implicitly takes on that role to carry out the actions (instead of executing sts:AssumeRole).
        The privileges attached to the role are distinct from those of the primary ordering the action and may even be larger and can cause security issues.
            `,
 		Links: []string{
