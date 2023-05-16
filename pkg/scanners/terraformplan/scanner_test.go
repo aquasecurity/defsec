022e8e0c3016f683ffae769fd3634891641dfe51
Simar <simar@linux.com>
2023-05-16 13:59:04 -0600

    fix tests
    Signed-off-by: Simar <simar@linux.com>

--- pkg/scanners/terraformplan/scanner_test.go
+++ pkg/scanners/terraformplan/scanner_test.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"os"
 	"testing"
 
-	"github.com/aquasecurity/defsec/pkg/scan"
 	"github.com/aquasecurity/defsec/pkg/scanners/options"
 	"github.com/aquasecurity/defsec/test/testutil"
 	"github.com/stretchr/testify/assert"
@@ -14,11 +14,9 @@ import (
 )
 
 func Test_OptionWithPolicyDirs_OldRegoMetadata(t *testing.T) {
-
+	b, _ := os.ReadFile("test/testdata/plan.json")
 	fs := testutil.CreateFS(t, map[string]string{
-		"/code/main.tfplan.json": `
-{"format_version":"1.1","terraform_version":"1.4.6","planned_values":{"root_module":{"resources":[{"address":"aws_s3_bucket.my-bucket","mode":"managed","type":"aws_s3_bucket","name":"my-bucket","provider_name":"registry.terraform.io/hashicorp/aws","schema_version":0,"values":{"bucket":"evil","force_destroy":false,"tags":null,"timeouts":null},"sensitive_values":{"cors_rule":[],"grant":[],"lifecycle_rule":[],"logging":[],"object_lock_configuration":[],"replication_configuration":[],"server_side_encryption_configuration":[],"tags_all":{},"versioning":[],"website":[]}}]}},"resource_changes":[{"address":"aws_s3_bucket.my-bucket","mode":"managed","type":"aws_s3_bucket","name":"my-bucket","provider_name":"registry.terraform.io/hashicorp/aws","change":{"actions":["create"],"before":null,"after":{"bucket":"evil","force_destroy":false,"tags":null,"timeouts":null},"after_unknown":{"acceleration_status":true,"acl":true,"arn":true,"bucket_domain_name":true,"bucket_prefix":true,"bucket_regional_domain_name":true,"cors_rule":true,"grant":true,"hosted_zone_id":true,"id":true,"lifecycle_rule":true,"logging":true,"object_lock_configuration":true,"object_lock_enabled":true,"policy":true,"region":true,"replication_configuration":true,"request_payer":true,"server_side_encryption_configuration":true,"tags_all":true,"versioning":true,"website":true,"website_domain":true,"website_endpoint":true},"before_sensitive":false,"after_sensitive":{"cors_rule":[],"grant":[],"lifecycle_rule":[],"logging":[],"object_lock_configuration":[],"replication_configuration":[],"server_side_encryption_configuration":[],"tags_all":{},"versioning":[],"website":[]}}}],"configuration":{"provider_config":{"aws":{"name":"aws","full_name":"registry.terraform.io/hashicorp/aws","expressions":{"profile":{"constant_value":"foo-bar-123123123"},"region":{"constant_value":"us-west-1"}}}},"root_module":{"resources":[{"address":"aws_s3_bucket.my-bucket","mode":"managed","type":"aws_s3_bucket","name":"my-bucket","provider_config_key":"aws","expressions":{"bucket":{"constant_value":"evil"}},"schema_version":0}]}}}
-`,
+		"/code/main.tfplan.json": string(b),
 		"/rules/test.rego": `
 package defsec.abcdefg
 
@@ -41,7 +39,7 @@ __rego_input__ := {
 
 deny[cause] {
 	bucket := input.aws.s3.buckets[_]
-	bucket.name.value == "evil"
+	bucket.name.value == "tfsec-plan-testing"
 	cause := bucket.name
 }
 `,
@@ -64,39 +62,6 @@ deny[cause] {
 	failure := results.GetFailed()[0]
 
 	assert.Equal(t, "AVD-TEST-0123", failure.Rule().AVDID)
-
-	actualCode, err := failure.GetCode()
-	require.NoError(t, err)
-	for i := range actualCode.Lines {
-		actualCode.Lines[i].Highlighted = ""
-	}
-	assert.Equal(t, []scan.Line{
-		{
-			Number:     1,
-			Content:    "resource \"aws_s3_bucket\" \"my-bucket\" {",
-			IsCause:    false,
-			FirstCause: false,
-			LastCause:  false,
-			Annotation: "",
-		},
-		{
-			Number:     2,
-			Content:    "\tbucket = \"evil\"",
-			IsCause:    true,
-			FirstCause: true,
-			LastCause:  true,
-			Annotation: "",
-		},
-		{
-			Number:     3,
-			Content:    "	}",
-			IsCause:    false,
-			FirstCause: false,
-			LastCause:  false,
-			Annotation: "",
-		},
-	}, actualCode.Lines)
-
 	if t.Failed() {
 		fmt.Printf("Debug logs:\n%s\n", debugLog.String())
 	}
@@ -104,11 +69,9 @@ deny[cause] {
 }
 
 func Test_OptionWithPolicyDirs_WithUserNamespace(t *testing.T) {
-
+	b, _ := os.ReadFile("test/testdata/plan.json")
 	fs := testutil.CreateFS(t, map[string]string{
-		"/code/main.tfplan.json": `
-{"format_version":"1.1","terraform_version":"1.4.6","planned_values":{"root_module":{"resources":[{"address":"aws_s3_bucket.my-bucket","mode":"managed","type":"aws_s3_bucket","name":"my-bucket","provider_name":"registry.terraform.io/hashicorp/aws","schema_version":0,"values":{"bucket":"evil","force_destroy":false,"tags":null,"timeouts":null},"sensitive_values":{"cors_rule":[],"grant":[],"lifecycle_rule":[],"logging":[],"object_lock_configuration":[],"replication_configuration":[],"server_side_encryption_configuration":[],"tags_all":{},"versioning":[],"website":[]}}]}},"resource_changes":[{"address":"aws_s3_bucket.my-bucket","mode":"managed","type":"aws_s3_bucket","name":"my-bucket","provider_name":"registry.terraform.io/hashicorp/aws","change":{"actions":["create"],"before":null,"after":{"bucket":"evil","force_destroy":false,"tags":null,"timeouts":null},"after_unknown":{"acceleration_status":true,"acl":true,"arn":true,"bucket_domain_name":true,"bucket_prefix":true,"bucket_regional_domain_name":true,"cors_rule":true,"grant":true,"hosted_zone_id":true,"id":true,"lifecycle_rule":true,"logging":true,"object_lock_configuration":true,"object_lock_enabled":true,"policy":true,"region":true,"replication_configuration":true,"request_payer":true,"server_side_encryption_configuration":true,"tags_all":true,"versioning":true,"website":true,"website_domain":true,"website_endpoint":true},"before_sensitive":false,"after_sensitive":{"cors_rule":[],"grant":[],"lifecycle_rule":[],"logging":[],"object_lock_configuration":[],"replication_configuration":[],"server_side_encryption_configuration":[],"tags_all":{},"versioning":[],"website":[]}}}],"configuration":{"provider_config":{"aws":{"name":"aws","full_name":"registry.terraform.io/hashicorp/aws","expressions":{"profile":{"constant_value":"foo-bar-123123123"},"region":{"constant_value":"us-west-1"}}}},"root_module":{"resources":[{"address":"aws_s3_bucket.my-bucket","mode":"managed","type":"aws_s3_bucket","name":"my-bucket","provider_config_key":"aws","expressions":{"bucket":{"constant_value":"evil"}},"schema_version":0}]}}}
-`,
+		"/code/main.tfplan.json": string(b),
 		"/rules/test.rego": `
 # METADATA
 # title: Bad buckets are bad
@@ -126,7 +89,7 @@ package user.foobar.ABC001
 
 deny[cause] {
 	bucket := input.aws.s3.buckets[_]
-	bucket.name.value == "evil"
+	bucket.name.value == "tfsec-plan-testing"
 	cause := bucket.name
 }
 `,
@@ -150,39 +113,6 @@ deny[cause] {
 	failure := results.GetFailed()[0]
 
 	assert.Equal(t, "AVD-TEST-0123", failure.Rule().AVDID)
-
-	actualCode, err := failure.GetCode()
-	require.NoError(t, err)
-	for i := range actualCode.Lines {
-		actualCode.Lines[i].Highlighted = ""
-	}
-	assert.Equal(t, []scan.Line{
-		{
-			Number:     1,
-			Content:    "resource \"aws_s3_bucket\" \"my-bucket\" {",
-			IsCause:    false,
-			FirstCause: false,
-			LastCause:  false,
-			Annotation: "",
-		},
-		{
-			Number:     2,
-			Content:    "\tbucket = \"evil\"",
-			IsCause:    true,
-			FirstCause: true,
-			LastCause:  true,
-			Annotation: "",
-		},
-		{
-			Number:     3,
-			Content:    "	}",
-			IsCause:    false,
-			FirstCause: false,
-			LastCause:  false,
-			Annotation: "",
-		},
-	}, actualCode.Lines)
-
 	if t.Failed() {
 		fmt.Printf("Debug logs:\n%s\n", debugLog.String())
 	}
