52922c50e9dba2675caad54099b1700ed208d845
Simar <simar@linux.com>
2023-05-16 13:43:38 -0600

    cleanup
    Signed-off-by: Simar <simar@linux.com>

--- pkg/detection/detect.go
+++ pkg/detection/detect.go
@@ -3,7 +3,6 @@ package detection
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"path/filepath"
 	"strings"
@@ -75,7 +74,6 @@ func init() {
 
 	matchers[FileTypeTerraform] = func(name string, _ io.ReadSeeker) bool {
 		ext := filepath.Ext(filepath.Base(name))
-		fmt.Println(">>> ext: ", ext)
 		return strings.EqualFold(ext, ".tf") || strings.EqualFold(ext, ".tf.json") || strings.EqualFold(ext, ".tfvars")
 	}
 
@@ -88,9 +86,8 @@ func init() {
 			contents := make(map[string]interface{})
 			err := json.NewDecoder(r).Decode(&contents)
 			if err == nil {
-				if tfv, ok := contents["terraform_version"]; ok {
-					fvv, stillOk := contents["format_version"]
-					fmt.Println(">>> tf ver:", tfv, "fv ver: ", fvv)
+				if _, ok := contents["terraform_version"]; ok {
+					_, stillOk := contents["format_version"]
 					return stillOk
 				}
 			}
