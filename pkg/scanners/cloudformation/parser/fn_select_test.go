9046b67f6454d6c668c372c4930da005398b0b33
Nikita Pivkin <100182843+nikpivkin@users.noreply.github.com>
2023-07-20 15:57:56 +0300

    fix(cloudformation): fix panic when use pseudo-parameters NoValue or NotificationARNs (#1395)
    * fix(cloudformation): fix panic when use pseudo-parameters NoValue or NotificationARNs
    
    * fix typo

--- pkg/scanners/cloudformation/parser/fn_select_test.go
+++ pkg/scanners/cloudformation/parser/fn_select_test.go
@@ -34,3 +34,44 @@ Resources:
 
 	require.Equal(t, "redis", engineProp.AsString())
 }
+
+func Test_SelectPseudoListParam(t *testing.T) {
+	src := `---
+Resources:
+  myASGrpOne:
+    Type: AWS::AutoScaling::AutoScalingGroup
+    Version: "2009-05-15"
+    Properties:
+      AvailabilityZones:
+        - "us-east-1a"
+      LaunchConfigurationName:
+        Ref: MyLaunchConfiguration
+      MinSize: "0"
+      MaxSize: "0"
+      NotificationConfigurations:
+        - TopicARN:
+            Fn::Select:
+              - "1"
+              - Ref: AWS::NotificationARNs
+      NotificationTypes:
+        - autoscaling:EC2_INSTANCE_LAUNCH
+        - autoscaling:EC2_INSTANCE_LAUNCH_ERROR
+
+`
+
+	ctx := createTestFileContext(t, src)
+	require.NotNil(t, ctx)
+
+	resource := ctx.GetResourceByLogicalID("myASGrpOne")
+	require.NotNil(t, resource)
+
+	notification := resource.GetProperty("NotificationConfigurations")
+	require.True(t, notification.IsNotNil())
+	require.True(t, notification.IsList())
+	first := notification.AsList()[0]
+	require.True(t, first.IsMap())
+	topic, ok := first.AsMap()["TopicARN"]
+	require.True(t, ok)
+	require.Equal(t, "notification::arn::2", topic.AsString())
+
+}
