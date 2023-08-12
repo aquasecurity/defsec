6a29d57d01d3dfafb8f1212a4e3b79f9658e7833
Nikita Pivkin <nikita.pivkin@smartforce.io>
2023-08-12 20:51:19 +0700

    fix: make operations with reference key safe

--- pkg/terraform/reference_test.go
+++ pkg/terraform/reference_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/zclconf/go-cty/cty"
 )
 
 func Test_ReferenceParsing(t *testing.T) {
@@ -46,6 +48,124 @@ func Test_ReferenceParsing(t *testing.T) {
 			ref, err := newReference(test.input, "")
 			assert.NoError(t, err)
 			assert.Equal(t, test.expected, ref.String())
+		})
+	}
+}
+
+func Test_SetKey(t *testing.T) {
+	tests := []struct {
+		name string
+		key  cty.Value
+		want cty.Value
+	}{
+		{
+			name: "happy",
+			key:  cty.StringVal("str"),
+			want: cty.StringVal("str"),
+		},
+		{
+			name: "null key",
+			key:  cty.NullVal(cty.String),
+			want: cty.Value{},
+		},
+		{
+			name: "unknown key",
+			key:  cty.UnknownVal(cty.String),
+			want: cty.Value{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Run(tt.name, func(t *testing.T) {
+				ref, err := newReference([]string{"resource", "test"}, "")
+				require.NoError(t, err)
+
+				ref.SetKey(tt.key)
+
+				assert.Equal(t, tt.want, ref.RawKey())
+			})
+		})
+	}
+}
+
+func Test_Key(t *testing.T) {
+
+	tests := []struct {
+		name string
+		key  cty.Value
+		want string
+	}{
+		{
+			name: "empty key",
+			want: "",
+		},
+		{
+			name: "str key",
+			key:  cty.StringVal("some_value"),
+			want: "some_value",
+		},
+		{
+			name: "number key",
+			key:  cty.NumberIntVal(122),
+			want: "122",
+		},
+		{
+			name: "bool key",
+			key:  cty.BoolVal(true),
+			want: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Run(tt.name, func(t *testing.T) {
+				ref, err := newReference([]string{"resource", "test"}, "")
+				require.NoError(t, err)
+
+				ref.SetKey(tt.key)
+
+				assert.Equal(t, tt.want, ref.Key())
+			})
+		})
+	}
+}
+
+func Test_KeyBracketed(t *testing.T) {
+	tests := []struct {
+		name string
+		key  cty.Value
+		want string
+	}{
+		{
+			name: "empty key",
+			want: "",
+		},
+		{
+			name: "str key",
+			key:  cty.StringVal("some_value"),
+			want: "[\"some_value\"]",
+		},
+		{
+			name: "number key",
+			key:  cty.NumberIntVal(122),
+			want: "[122]",
+		},
+		{
+			name: "bool key",
+			key:  cty.BoolVal(true),
+			want: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ref, err := newReference([]string{"resource", "test"}, "")
+			require.NoError(t, err)
+
+			ref.SetKey(tt.key)
+
+			assert.Equal(t, tt.want, ref.KeyBracketed())
 		})
 	}
 }
