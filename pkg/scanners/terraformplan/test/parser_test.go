014fce26b14e94bc32d5815b1fec225e64121d6e
dorayakikun <amakunai.dorayaki@gmail.com>
2023-09-16 09:01:09 +0900

    feat: Add map slices testing

--- pkg/scanners/terraformplan/test/parser_test.go
+++ pkg/scanners/terraformplan/test/parser_test.go
@@ -1,11 +1,12 @@
 package terraformplan
 
 import (
-	"testing"
-
 	"github.com/aquasecurity/defsec/pkg/scanners/terraformplan/parser"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func Test_Parse_Plan_File(t *testing.T) {
@@ -18,4 +19,20 @@ func Test_Parse_Plan_File(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.NotNil(t, fs)
+}
+
+func Test_Parse_Plan_File_Include_Map_Slice(t *testing.T) {
+
+	planFile, err := parser.New().ParseFile("testdata/plan_include_map_slice.json")
+	require.NoError(t, err)
+
+	assert.NotNil(t, planFile)
+	fs, err := planFile.ToFS()
+	require.NoError(t, err)
+
+	assert.NotNil(t, fs)
+
+	file, err := fs.ReadFile("main.tf")
+	_, diags := hclsyntax.ParseTemplate(file, "main.tf", hcl.Pos{Line: 1, Column: 1})
+	assert.False(t, diags.HasErrors())
 }
