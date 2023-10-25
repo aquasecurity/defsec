38814be2885725cec2be8305475e57e64d422f5b
Nikita Pivkin <nikita.pivkin@smartforce.io>
2023-10-26 00:11:07 +0700

    feat(terraform): override ID for some IAM resources

--- pkg/terraform/presets.go
+++ pkg/terraform/presets.go
@@ -4,10 +4,13 @@ import (
 	"strings"
 
 	"github.com/zclconf/go-cty/cty"
+	"golang.org/x/exp/slices"
 )
 
-func createPresetValues(b *Block) map[string]cty.Value {
-	presets := make(map[string]cty.Value)
+type values map[string]cty.Value
+
+func createPresetValues(b *Block) values {
+	presets := make(values)
 
 	// here we set up common "id" values that are set by the provider - this ensures all blocks have a default
 	// referencable id/arn. this isn't perfect, but the only way to link blocks in certain circumstances.
@@ -27,16 +30,23 @@ func createPresetValues(b *Block) map[string]cty.Value {
 
 }
 
-func postProcessValues(b *Block, input map[string]cty.Value) map[string]cty.Value {
+func postProcessValues(b *Block, input values) values {
 
 	// alias id to "bucket" (bucket name) for s3 bucket resources
 	if strings.HasPrefix(b.TypeLabel(), "aws_s3_bucket") {
-		if bucket, ok := input["bucket"]; ok {
-			input["id"] = bucket
-		} else {
-			input["bucket"] = cty.StringVal(b.ID())
-		}
+		overrideIdOrSetAttr("bucket", input, b)
+	} else if slices.Contains([]string{"aws_iam_role", "aws_iam_group", "aws_iam_user"}, "") {
+		// The resource ID is equal to the name attribute
+		// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role#id
+		overrideIdOrSetAttr("name", input, b)
 	}
-
 	return input
+}
+
+func overrideIdOrSetAttr(attributeName string, input values, b *Block) {
+	if name, ok := input[attributeName]; ok {
+		input["id"] = name
+	} else {
+		input[attributeName] = cty.StringVal(b.ID())
+	}
 }
