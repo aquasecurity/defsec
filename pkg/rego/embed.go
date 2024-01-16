dd5b541b58f9ebeb314f36f3a561b8b25ffa5276
Nikita Pivkin <nikita.pivkin@smartforce.io>
2024-01-17 03:56:41 +0600

    refactor: fix path to optional rules (#1524)
    fix path to optional rules

--- pkg/rego/embed.go
+++ pkg/rego/embed.go
@@ -79,7 +79,7 @@ func LoadPoliciesFromDirs(target fs.FS, paths ...string) (map[string]*ast.Module
 				return nil
 			}
 
-			if strings.HasSuffix(filepath.Dir(filepath.ToSlash(path)), "policies/advanced/optional") {
+			if strings.HasSuffix(filepath.Dir(filepath.ToSlash(path)), "advanced/optional") {
 				return fs.SkipDir
 			}
 
